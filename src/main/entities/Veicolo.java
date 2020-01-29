a2cd581fb65974ee619864df147f286090018f75
unknown <stefano.covalero@si2001.it>
2020-01-29 17:34:40 +0100

    first commit

---
+++ src/main/entities/Veicolo.java
@@ -0,0 +1,12 @@
+package main.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "veicolo")
+public class Veicolo {
+    public Veicolo(){}
+
+
+}
